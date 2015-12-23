bd1a18b1458d39eca6fbd748be8ae98521450191
juthebest <judetyr@gmail.com>
2015-12-23 18:58:58 +1100

    trying to convert to html5 for navigations link but failed

---
+++ webapps/bootstrap-3.3.5-dist/navlinks.js
@@ -0,0 +1,42 @@
+
+document.getElementById("nav01").innerHTML =
+
+	"<ul class="nav navbar-nav navbar-right">"+
+					
+"<li><a href='index.html'>Home</a></li>" + 
+					"<li><a href='about.html'>About</a></li>" +
+					"<li><a href='viewProgramsNcourses.html'>Courses & Programs</a></li>" +
+					"<li><a href='ContactUs.html'>Contact Us</a></li>" +
+					"<li><a href='#login' data-toggle='modal'>Log In</a></li>" +
+
+					<!--  l#login is for reference for jquery, #data-toggle to triggle function -->
+
+					<!-- add search form -->
+					"<li id='search'>" +
+						"<form class='navbar-form navbar-right' role='search'>" +
+							"<div class='input-group'>" +
+								"<input type='text' class='form-control' placeholder='Search '>" +
+								"<span class='input-group-btn'>" +
+									"<button type='submit' class='btn btn-default'>" +
+										"<span class='glyphicon glyphicon-search'></span>" +
+									"</button>" +
+							"</span>" +
+							"</div>" +
+						"</form>" +
+					"</li>" +
+					<!-- end of add search form -->
+
+					<!-- shopping cart -->
+				"<li class='dropdown'>" +
+					"<a href='shoppingcart.html' class='dropdown-toggle' data-toggle='dropdown'>" +
+						"<span class='glyphicon glyphicon-shopping-cart'>" +</span>" +
+						"<b class='caret'>" +
+							"</b>" +
+					"</a>" +
+
+					"<ul class='dropdown-menu'>" +
+							"<li><a href='shoppingcart.html'>Bag</a></li>" +
+							"<li><a href='#'>Checkout</a></li>" +
+						"</ul></li>" +
+					<!-- end of shopping cart -->
+		"</ul>"	;		
