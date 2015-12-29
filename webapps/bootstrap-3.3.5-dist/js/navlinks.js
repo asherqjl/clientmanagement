50d15e9b5258550020cc62f0a0724a953e9e96a3
juthebest <judetyr@gmail.com>
2015-12-29 20:58:04 +1100

    change navigation bar for index page to html5

--- webapps/bootstrap-3.3.5-dist/js/navlinks.js
+++ webapps/bootstrap-3.3.5-dist/js/navlinks.js
@@ -1,44 +1,38 @@
-<link href="css/home.css" rel="stylesheet">
-<script>
-document.getElementById("nav01").innerHTML =
 
-	"<ul class="nav navbar-nav navbar-right">"+
-					
-"<li><a href='index.html'>Home</a></li>" + 
-					"<li><a href='about.html'>About</a></li>" +
-					"<li><a href='viewProgramsNcourses.html'>Courses & Programs</a></li>" +
-					"<li><a href='ContactUs.html'>Contact Us</a></li>" +
-					"<li><a href='#login' data-toggle='modal'>Log In</a></li>" +
+document.getElementById("navMenu").innerHTML =
 
-					<!--  l#login is for reference for jquery, #data-toggle to triggle function -->
 
-					<!-- add search form -->
-					"<li id='search'>" +
-						"<form class='navbar-form navbar-right' role='search'>" +
-							"<div class='input-group'>" +
-								"<input type='text' class='form-control' placeholder='Search '>" +
-								"<span class='input-group-btn'>" +
-									"<button type='submit' class='btn btn-default'>" +
-										"<span class='glyphicon glyphicon-search'></span>" +
-									"</button>" +
-							"</span>" +
-							"</div>" +
-						"</form>" +
-					"</li>" +
-					<!-- end of add search form -->
+	'<div class="navbar navbar-default navbar-fixed-top">'+
+		'<div class="container">'+
+			'<div class="navbar-header">'+
+				'<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'+
+					'<span class="sr-only">Toggle navigation</span>'+
+					' <span class="icon-bar"></span>'+
+					'<!-- for the responsive icon bar when page shrink -->'+
+					'<span class="icon-bar"></span>'+
+					'<!-- for the responsive icon bar when page shrink -->'+
+					'<span class="icon-bar"></span>'+
+					'<!-- for the responsive icon bar when page shrink -->'+
+				'</button>'+
 
-					<!-- shopping cart -->
-				"<li class='dropdown'>" +
-					"<a href='shoppingcart.html' class='dropdown-toggle' data-toggle='dropdown'>" +
-						"<span class='glyphicon glyphicon-shopping-cart'>" +</span>" +
-						"<b class='caret'>" +
-							"</b>" +
-					"</a>" +
-
-					"<ul class='dropdown-menu'>" +
-							"<li><a href='shoppingcart.html'>Bag</a></li>" +
-							"<li><a href='#'>Checkout</a></li>" +
-						"</ul></li>" +
-					<!-- end of shopping cart -->
-		"</ul>"			
-	</script>
+				'<a class="navbar-brand" href="index.html"> <img id="brand-image" alt="Website logo" src="image/brandimage.PNG" /> I*MPOSSIBLE'+
+				'</a>'+
+'</div>'+
+'	<div class="navbar-collapse collapse">'+
+ '<ul class="nav navbar-nav navbar-right">'+
+  '<li><a href="index.html">Home</a></li>'+
+  '<li><a href="viewProgramsNcourses.html">Courses and Programs</a></li>'+
+  '<li><a href="about.html">About</a></li>'+
+   '<li><a href="ContactUs.html">Contact Us</a></li>'+
+      '<li><a href="#login" data-toggle="modal">Log In</a></li>'+
+		   '<li><form class="navbar-form navbar-right" role="search">'+
+						'<div class="input-group">'+
+						'	<input type="text" class="form-control" placeholder="Search ">'+
+							'<span class="input-group-btn">'+
+								'<button type="submit" class="btn btn-default">'+
+									'<span class="glyphicon glyphicon-search"></span>'+
+								'</button>'+
+							'</span>'+
+						'</div>'+
+					'</form></li>'+
+ '</ul>';
