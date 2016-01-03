75493755b3adb2f80b0586b4221a196d554b9709
Darius Koh Shi Rui <Darius Koh Shi Rui>
2016-01-03 11:51:51 +0800

    adsfdgsd

--- webapps/bootstrap-3.3.5-dist/js/footer.js
+++ webapps/bootstrap-3.3.5-dist/js/footer.js
@@ -1,16 +1,14 @@
-	document.getElementById("footerMenu").innerHTML =
-	'<div class="navbar navbar-inverse navbar-fixed-bottom">'+
-		'<div class="container">'+
-			'<div class="navbar-text pull-left">'+
-				'<div style="color: white">'+
-					'<p>&#169;COPYRIGHT 2015 I*MPOSSIBLE</p>'+
-				'</div>'+
-			'</div>'+
-			'<div class="navbar-text pull-right">'+
-				'<a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>'+
-				'<a href="#"><i class="fa fa-twitter-square fa-2x"></i></a> '+
-				'<a href="#"><i class="fa fa-google-plus fa-2x"></i></a> '+
-				'<a href="#"><i class="fa fa-linkedin-square fa-2x"></i></a>'+
-			'</div>'+
-		'</div>'+
-	'</div>';
+document.getElementById("footerMenu").innerHTML =
+
+'<div class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">'
+		+ '		<div class="container">'
+		+ '			<div class="navbar-text pull-left">'
+		+ '				<p>©COPYRIGHT 2015 I\'MPOSSIBLE</p>'
+		+ '			</div>'
+		+ '			<div class="navbar-text pull-right">'
+		+ '				<a href="#"><i class="fa fa-facebook-square fa-2x"></i></a> <a'
+		+ '					href="#"><i class="fa fa-twitter-square fa-2x"></i></a> <a href="#"><i'
+		+ '					class="fa fa-google-plus fa-2x"></i></a> <a href="#"><i'
+		+ '					class="fa fa-linkedin-square fa-2x"></i></a>' + '			</div>'
+		+ '		</div>' + '	</div>';
+		
