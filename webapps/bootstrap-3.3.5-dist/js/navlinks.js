9e9719c17c2bee05a4f6530f3a4661590fe277a9
juthebest <judetyr@gmail.com>
2016-01-03 12:13:25 +0800

    footer.js

--- webapps/bootstrap-3.3.5-dist/js/navlinks.js
+++ webapps/bootstrap-3.3.5-dist/js/navlinks.js
@@ -3,52 +3,52 @@ document.getElementById("navMenu").innerHTML =
 
 
 	'<div class="navbar navbar-default navbar-fixed-top">'+
-		'<div class="container">'+
-			'<div class="navbar-header">'+
-				'<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'+
-					'<span class="sr-only">Toggle navigation</span>'+
-					' <span class="icon-bar"></span>'+
-					'<!-- for the responsive icon bar when page shrink -->'+
-					'<span class="icon-bar"></span>'+
-					'<!-- for the responsive icon bar when page shrink -->'+
-					'<span class="icon-bar"></span>'+
-					'<!-- for the responsive icon bar when page shrink -->'+
-				'</button>'+
+	'<div class="container">'+
+	'<div class="navbar-header">'+
+	'<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">'+
+	'<span class="sr-only">Toggle navigation</span>'+
+	' <span class="icon-bar"></span>'+
+	'<!-- for the responsive icon bar when page shrink -->'+
+	'<span class="icon-bar"></span>'+
+	'<!-- for the responsive icon bar when page shrink -->'+
+	'<span class="icon-bar"></span>'+
+	'<!-- for the responsive icon bar when page shrink -->'+
+	'</button>'+
 
-				'<a class="navbar-brand" href="index.html"> <img id="brand-image" alt="Website logo" src="image/brandimage.PNG" /> I*MPOSSIBLE'+
-				'</a>'+
-'</div>'+
-'	<div class="navbar-collapse collapse">'+
- '<ul class="nav navbar-nav navbar-right">'+
-  '<li><a href="index.html">Home</a></li>'+
-  '<li><a href="about.html">About</a></li>'+
-    '<li><a href="viewProgramsNcourses.html">Courses and Programs</a></li>'+
-   '<li><a href="ContactUs.html">Contact Us</a></li>'+
-      '<li><a href="#login" data-toggle="modal">Log In</a></li>'+
-		   '<li><form class="navbar-form navbar-right" role="search">'+
-						'<div class="input-group">'+
-						'	<input type="text" class="form-control" placeholder="Search ">'+
-							'<span class="input-group-btn">'+
-								'<button type="submit" class="btn btn-default">'+
-									'<span class="glyphicon glyphicon-search"></span>'+
-								'</button>'+
-							'</span>'+
-						'</div>'+
-					'</form></li>'+
+	'<a class="navbar-brand" href="index.html"> <img id="brand-image" alt="Website logo" src="image/brandimage.PNG" /> I*MPOSSIBLE'+
+	'</a>'+
+	'</div>'+
+	'	<div class="navbar-collapse collapse">'+
+	'<ul class="nav navbar-nav navbar-right">'+
+	'<li><a href="index.html">Home</a></li>'+
+	'<li><a href="about.html">About</a></li>'+
+	'<li><a href="viewProgramsNcourses.html">Courses and Programs</a></li>'+
+	'<li><a href="ContactUs.html">Contact Us</a></li>'+
+	'<li><a href="#login" data-toggle="modal">Log In</a></li>'+
+	'<li><form class="navbar-form navbar-right" role="search">'+
+	'<div class="input-group">'+
+	'	<input type="text" class="form-control" placeholder="Search ">'+
+	'<span class="input-group-btn">'+
+	'<button type="submit" class="btn btn-default">'+
+	'<span class="glyphicon glyphicon-search"></span>'+
+	'</button>'+
+	'</span>'+
+	'</div>'+
+	'</form></li>'+
 
-					
 
- '<li class="dropdown"><a href="shoppingcart.html"'+
-'						class="dropdown-toggle" data-toggle="dropdown"> <span'+
-'							class="glyphicon glyphicon-shopping-cart"></span> <span class="badge">2</span><b'+
-'							class="caret"></b>'+
-'					</a>'+
-'						<ul class="dropdown-menu">'+
-'							<li><a href="shoppingcart.html">Bag</a></li>'+
-'							<li><a href="#">Checkout</a></li>'+
-'						</ul></li>'+
- '</ul>'+
- 
- 			'</div>'+
-		'</div>'+
+
+	'<li class="dropdown"><a href="shoppingcart.html"'+
+	'						class="dropdown-toggle" data-toggle="dropdown"> <span'+
+	'							class="glyphicon glyphicon-shopping-cart"></span> <span class="badge">2</span><b'+
+	'							class="caret"></b>'+
+	'					</a>'+
+	'						<ul class="dropdown-menu">'+
+	'							<li><a href="shoppingcart.html">Bag</a></li>'+
+	'							<li><a href="#">Checkout</a></li>'+
+	'						</ul></li>'+
+	'</ul>'+
+
+	'</div>'+
+	'</div>'+
 	'</div>'	;
